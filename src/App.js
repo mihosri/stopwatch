4eeb8f00d973ac979ffeb687a043516e6a437261
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-11-08 18:11:52 -0700

    implemented use-timer hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import {useTimer} from "use-timer";
 import './App.css';
 
 function App() {
+
+  const {time, start, pause, reset, status} = useTimer();
   return (
     <>
-      
+      <div>
+        <button onClick={start}>Start</button>
+        <button onClick={pause}>Pause</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <p>Start Timer: {time}</p>
+      {status === 'RUNNING' && <p>Running...</p>}
     </>
   );
 }
